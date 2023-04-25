ef80e81cf3e985fdc2b6d7300a6d1a6ef96172d1
Alan P Aby <alanaby01@gmail.com>
2023-04-25 12:55:49 -0400

    GET from tmdb database

--- src/Banner.js
+++ src/Banner.js
@@ -1,25 +1,49 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './Banner.css';
+import instance from './axios';
+import requests from './Requests';
 
-function Banner(props) {
-    return (
-        <div className='banner' style={{
-            backgroundSize: "cover",
-            backgroundImage: `url('https://pixy.org/src/49/thumbs350/491517.jpg')`
-            }}
-        >
-        <div className='banner__contents'>
-            <div className='banner__title'>Movie Name</div>
-            <div className='banner__buttons'>
-                <button className='banner__button'>Play</button>
-                <button className='banner__button'>My list</button>
-            </div>
-            <h1 className='banner_decsription'>This is a test description</h1>
-        </div>
-        <div className='banner--fadeBottom'></div>
+function Banner() {
 
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData(){
+            const request = await instance.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            )
+            return request;
             
-        </div>
+        }
+        fetchData();
+    }, []);
+    console.log(movie);
+
+    function truncate(string, n){
+        return string?.length > n ? string.substr(0, n-1) + '...' : string;
+    }
+    return (
+        <header className='banner' style={{
+            backgroundSize: "cover",
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+            backgroundPosition: "center center"
+        }}
+        >
+            <div className='banner__contents'>
+                <h1 className='banner__title'>{movie?.original_name}</h1>
+                <div className='banner__buttons'>
+                    <button className='banner__button'>Play</button>
+                    <button className='banner__button'>My list</button>
+                </div>
+                <h1 className='banner__decsription'>{truncate(`${movie?.overview}`, 300)}   
+                </h1>
+            </div>
+            <div className='banner--fadeBottom'></div>
+        </header>
     );
 }
 
---
+++ src/Requests.js
@@ -0,0 +1,13 @@
+const API_KEY = "fb548dd62b8ee6843d4ddca17afe6acf";
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+};
+
+export default requests;
