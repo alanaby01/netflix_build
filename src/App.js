d5ad3ce31ddd6f5d1bf61fe633cd6cd667f968e5
Alan P Aby <alanaby01@gmail.com>
2023-05-10 12:44:40 -0400

    Added routing components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeScreen></HomeScreen>,
+  },
+  {
+    path: "/test",
+    element: <div><text>This is Alan</text></div>
+  },
+
+]);
 
 function App() {
   return (
-    <div className="app">
-      <HomeScreen></HomeScreen>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
