3f1bad1d5f50c7bdef080443c9651bcc9e082544
Alan P Aby <alanaby01@gmail.com>
2023-05-24 13:52:40 -0400

    PlanScreen component

--- src/App.js
+++ src/App.js
@@ -2,29 +2,31 @@ import React, { useEffect } from 'react';
 import HomeScreen from './screens/HomeScreen';
 import './App.css';
 import LoginScreen from './screens/LoginScreen';
+import ProfileScreen from './screens/ProfileScreen';
 import {
-  createBrowserRouter,
-  RouterProvider,
+  BrowserRouter as Router,
+  Routes,
+  Route
 } from "react-router-dom";
 import { auth } from './firebase';
 import { useDispatch, useSelector } from 'react-redux';
 import { login, logout, selectUser } from './features/userSlice'
 
-const router = createBrowserRouter([
-  { 
-    path: "/",
-    element: <HomeScreen></HomeScreen>,
-  },
-  {
-    path: "/test",
-    element: <div><text>This is Alan</text></div>
-  },
-  {
-    path: "/login",
-    element: <LoginScreen></LoginScreen>
-  },
+// const router = createBrowserRouter([
+//   { 
+//     path: "/",
+//     element: <HomeScreen></HomeScreen>,
+//   },
+//   {
+//     path: "/test",
+//     element: <div><text>This is Alan</text></div>
+//   },
+//   {
+//     path: "/login",
+//     element: <LoginScreen></LoginScreen>
+//   },
 
-]);
+// ]);
 
 function App() {
   const user = useSelector(selectUser);
@@ -42,21 +44,26 @@ function App() {
       }
       else {
         //Logged out  
-        dispatch(logout);
+        dispatch(logout());
       }
     });
 
     return unsubscribe;
-  }, []);
+  }, [dispatch]);
 
   return (
     <>
       <div className='app'>
-        {!user ? (
-          <LoginScreen></LoginScreen>
-        ) : (
-          <HomeScreen></HomeScreen>
-        )}
+        <Router>
+          {!user ? (
+            <LoginScreen></LoginScreen>
+          ) : (
+            <Routes>
+              <Route path="/profile" element={<ProfileScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          )}
+        </Router>
       </div>
       {/* <RouterProvider router={router} /> */}
     </>
