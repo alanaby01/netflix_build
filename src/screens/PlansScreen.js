1d8778fbb82d0653a0caa407000217b3030747aa
Alan P Aby <alanaby01@gmail.com>
2023-06-19 19:58:44 -0400

    Data returns from Firebase for PlanScreen. UI in progress

--- src/screens/PlansScreen.js
+++ src/screens/PlansScreen.js
@@ -6,37 +6,21 @@ import { collection, query, where, getDocs } from "firebase/firestore";
 
 function PlansScreen() {
     const [products, setProducts] = useState([]);
-        
     useEffect(() => {
-        const fetchData = () => {
             const q = query(collection(db, "products"), where("active", "==", true));
             const querySnapshot = getDocs(q);
             querySnapshot.then((docs) => {
                 docs.forEach((doc) => {
                     console.log(doc.id, " => ", doc.data());
+                    products[doc.id] =  {
+                        priceId: doc.id,
+                        priceData: doc.data()
+                    }
             });
+            setProducts(products);
         });
-    };
-    fetchData().catch(console.error);
     }, [])
-    //     db.collection("products").where("active", "==", true)
-    //     .get()
-    //     .then((querySnapshot) => {
-    //         const products = {};
-    //         querySnapshot.forEach(async productDoc => {
-    //             products[productDoc.id] = productDoc.data();
-    //             const priceSnap = await productDoc.ref.collection("prices").get();
-    //             priceSnap.docs.forEach(price => {
-    //                 products[productDoc.id].prices = {
-    //                     priceId: price.id,
-    //                     priceData: price.data()
-    //                 }
-    //             })
-    //         });
-    //         setProducts(products);
-    //     });
-    // }, []);
-    // console.log(products);
+    console.log(products);
     return (
         <div className='plansScreen'>
             <h1>Plans</h1>
